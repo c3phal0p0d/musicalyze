1929c65b6993a2435aa60bb4aa158810bd424397
nchiorsac <nchiorsac@student.unimelb.edu.au>
2023-07-24 12:25:43 +1000

    restructured code & added jwt tokens

--- server/index.js
+++ server/index.js
@@ -1,56 +1,39 @@
-require("dotenv").config();
+const dotenv = require("dotenv");
+const express = require("express");
+const session = require("cookie-session");
+const helmet = require("helmet");
+const hpp = require("hpp");
+const csurf = require("csurf");
+const path = require("path");
+
+dotenv.config({path: path.resolve(__dirname, '.env')});
 
 const PORT = 3000;
-const URI = `http://localhost:${PORT}`
-const REDIRECT_URI = `http://localhost:${PORT}/account`
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const scope = "user-read-recently-played user-top-read"
+const COOKIE_SECRET = process.env.COOKIE_SECRET;
 
-const express = require("express");
-const querystring = require("querystring");
-const axios = require("axios");
-
+/* Create app */
 const app = express();
+
+/* Set up security configs */
+app.use(helmet());
+app.use(hpp());
+
+/* Set up cookie settings */
+app.use(
+    session({
+        name: "session",
+        keys: ["secret"],
+        secret: COOKIE_SECRET,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+    })
+);
+app.use(csurf());
+
+const authRoutes = require("./routes/auth");
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}`);
 });
 
-app.get("/", (req, res) => {
-    res.send(
-        `<a href="https://accounts.spotify.com/authorize?${querystring.stringify({
-            client_id: CLIENT_ID,
-            response_type: 'code',
-            redirect_uri: REDIRECT_URI,
-            scope: scope,
-        })}">Sign in</a>`
-    );
-});
-
-app.get("/account", async (req, res) => {
-    const authResponse = await axios.post(
-        "https://accounts.spotify.com/api/token",
-        querystring.stringify({
-            grant_type: 'authorization_code',
-            code: req.query.code,
-            redirect_uri: REDIRECT_URI,
-        }),
-        {
-            headers: {
-                Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-        }
-    );
-    console.log(authResponse.data);
-    const data = await axios.get(
-        "https://api.spotify.com/v1/me/top/tracks?limit=50",
-        {
-            headers: {
-                Authorization: "Bearer " + authResponse.data.access_token,
-            },
-        }
-    );
-    console.log(data);
-});
+module.exports = app;
---
+++ server/routes/auth.js
@@ -0,0 +1,74 @@
+const dotenv = require("dotenv");
+const express = require('express');
+const querystring = require('querystring');
+const axios = require('axios');
+const jwt = require('jsonwebtoken');
+const path = require("path");
+
+dotenv.config();
+
+const PORT = 3000;
+const URI = `http://localhost:${PORT}`
+const REDIRECT_URI = `http://localhost:${PORT}/auth/callback`
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const scope = "user-read-recently-played user-top-read";
+
+const router = express.Router();
+
+router.get("/login", (req, res) => {
+    console.log(CLIENT_ID);
+    console.log(REDIRECT_URI);
+    res.redirect(`https://accounts.spotify.com/authorize?${querystring.stringify({
+        client_id: CLIENT_ID,
+        response_type: 'code',
+        redirect_uri: REDIRECT_URI,
+        scope: scope,
+    })}`);
+});
+
+router.get("/callback", async (req, res) => {
+    const {code} = req.query;
+
+    const {data} = await axios.post(
+        "https://accounts.spotify.com/api/token",
+        querystring.stringify({
+            grant_type: 'authorization_code',
+            code: req.query.code,
+            redirect_uri: REDIRECT_URI,
+        }),
+        {
+            headers: {
+                Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        }
+    );
+
+    console.log(data);
+
+    const sessionJWTObject = {
+        token: data.access_token,
+    };
+
+    req.session.jwt = jwt.sign(sessionJWTObject, JWT_SECRET_KEY);
+    return res.redirect('/');
+});
+
+router.get('/current-session', (req, res) => {
+    jwt.verify(req.session.jwt, JWT_SECRET_KEY, (err, decodedToken) => {
+        if (err || !decodedToken) {
+            res.send(false);
+        } else {
+            res.send(decodedToken);
+        }
+    });
+});
+
+router.get("/logout", (req, res) => {
+    req.session = null;
+    res.redirect('/');
+});
+
+module.exports = router;
